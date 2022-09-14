d2930a858410a70894be46f9dd8d729f3f73ea29
FarxodElamasov <farxodflbbank@gmail.com>
2022-09-14 17:52:18 +0500

    Malulm

--- richard-demo-project/src/App.js
+++ richard-demo-project/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
   <div>
     <h1>Hello world</h1>
+    <h3>Salom git push</h3>
+    <h4>Salom git add</h4>
+    <Header />
   </div>
   );
 }
