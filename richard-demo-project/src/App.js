697d45072a070401a8a46dbeea6d058f1024bf05
Dilrux2007 <urazaliyevdilrux@gmail.com>
2022-09-18 06:57:36 +0500

    asdf

--- richard-demo-project/src/App.js
+++ richard-demo-project/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+
+
 
 function App() {
+<Navbar/>
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col-8">
             <h1>Hamma ulashlarni ulab qo'ydim, menimcha...</h1>
+            
           </div>
         </div>
       </div>
