0f5da9e42dc8b65569ce219dbcb043bc803a40f4
umidkhan <gameblogger924@gmail.com>
2022-09-21 17:08:35 +0500

    umidxon oldi

---
+++ richard-demo-project/src/Section/Section.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Burger from "../img/img1.png";
+
+export default function Section() {
+    return (
+        <>
+            <p>Aenean Consectetur Porta</p>
+            <h4>Sem Risus Elit Vestibulum</h4>
+            <p>Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+            <img src={Burger} alt="png" />
+        </>
+    )
+}
